f82c3274dc94a843405389311d577f6f919424a2
Ruben Bromée <tharubb@gmail.com>
2022-02-01 14:02:28 +0100

    JSON creation base

--- data_processing.py
+++ data_processing.py
@@ -1,18 +1,35 @@
 import json
 import datetime
+import random
 
-data = {
-    "name": "david",
-    "pn": "000000000",
-    "dates": [
+names = {"David", "Greg", "Heidi", "Janice", "Jeff"}
+measured_days = 31
 
-    ]
-}
+# Persons
+for idx, name in enumerate(names):
+    date = datetime.datetime(2022, 1, 1, 12, 0, 0)
+    data = {
+        "age": random.randint(20,40),
+        "bodyweight": random.randint(60,90),
+        "lifestyle_log": [
 
-date = datetime.datetime(2022, 1, 1, 12, 4, 5)
+        ],
+        "training_log": [
 
-for i in range(31):
-    data["dates"].append(date.date().isoformat())
-    date += datetime.timedelta(days = 1)
+        ]
+    }
 
-print(data)
+    for i in range(measured_days):
+        data["lifestyle_log"].append(
+        {
+            "date": date.date().isoformat(),
+            "energy_level": random.randint(1,10),
+            "water_drank": random.randint(1,5),
+            "stress_level": random.randint(1,10),
+            "motivation": random.randint(1,10),
+            "hours_slept": random.randint(5,12),
+            "caffein_intake": random.randint(0,400)
+        })
+        date += datetime.timedelta(days = 1)
+    out_file = open('' + name + '.json', 'w')
+    json.dump(data, out_file)
