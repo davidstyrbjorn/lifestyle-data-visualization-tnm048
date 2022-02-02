1377d4848e65df01ba73936fc9ba153f1f757bd2
Ruben Bromée <tharubb@gmail.com>
2022-02-02 14:06:18 +0100

    Started on data selection

--- info-vis-data-processing/pmdata-processing.py
+++ info-vis-data-processing/pmdata-processing.py
@@ -1,16 +1,40 @@
 import pandas
-
+import datetime
 
 N = 16
 
 for i in range(1,N+1):
 
-    # get the correct person id
+    # Get the correct person id
     person_id = "p"
     if(i <= 9): person_id += ('0' + str(i))
     else: person_id += (str(i))
 
-    # open pmsys data (wellness)
+    # Open pmsys data (wellness)
     pmsys_wellness = pandas.read_csv('pmdata/' + person_id + "/pmsys/wellness.csv")
+    pmsys_googledocs = pandas.read_csv('pmdata/' + person_id + "/googledocs/reporting.csv")
     
-    break
+    data = {
+        "lifestyle_log": [
+            
+        ],
+        "training_log": [
+
+        ]
+    }
+
+    measured_days = len(pmsys_wellness)
+
+    for i in range(measured_days):
+        data['lifestyle_log'].append({
+            "date": pmsys_wellness['effective_time_frame'][i][0:10],
+            "fatigue": pmsys_wellness['fatigue'][i],
+            "mood": pmsys_wellness['mood'][i],
+            "readiness": pmsys_wellness['readiness'][i],
+            "sleep_duration_h": pmsys_wellness['sleep_duration_h'][i],
+            "sleep_quality": pmsys_wellness['sleep_quality'][i],
+            "stress": pmsys_wellness['stress'][i]
+        })
+
+
+    print(data)
