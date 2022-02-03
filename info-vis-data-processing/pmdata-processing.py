a0d3ad36ffa2f1a7c433138cfb470f67a2a67cdb
Ruben Bromée <tharubb@gmail.com>
2022-02-03 10:12:45 +0100

    Google docs data start

--- info-vis-data-processing/pmdata-processing.py
+++ info-vis-data-processing/pmdata-processing.py
@@ -12,10 +12,14 @@ for i in range(1,N+1):
 
     # Open pmsys data (wellness)
     pmsys_wellness = pandas.read_csv('pmdata/' + person_id + "/pmsys/wellness.csv")
-    pmsys_googledocs = pandas.read_csv('pmdata/' + person_id + "/googledocs/reporting.csv")
-    
+    googledocs_reporting = pandas.read_csv('pmdata/' + person_id + "/googledocs/reporting.csv")
+
+    # Data structure for each person    
     data = {
-        "lifestyle_log": [
+        "wellness": [
+            
+        ],
+        "reporting": [
             
         ],
         "training_log": [
@@ -23,10 +27,12 @@ for i in range(1,N+1):
         ]
     }
 
-    measured_days = len(pmsys_wellness)
+    measured_days_wellness = len(pmsys_wellness)
+    measured_days_googledocs = len(googledocs_reporting)
 
-    for i in range(measured_days):
-        data['lifestyle_log'].append({
+    # Add measured lifestyle data for each measured day, combine with google docs data. Include dates where both files have data.
+    for i in range(measured_days_wellness):
+        data['wellness'].append({
             "date": pmsys_wellness['effective_time_frame'][i][0:10],
             "fatigue": pmsys_wellness['fatigue'][i],
             "mood": pmsys_wellness['mood'][i],
@@ -35,6 +41,18 @@ for i in range(1,N+1):
             "sleep_quality": pmsys_wellness['sleep_quality'][i],
             "stress": pmsys_wellness['stress'][i]
         })
+    
+    # Make separate list for things which are not measured in the same time frame as the wellness data
+    # Check both dates in each row
+    for i in range(measured_days_googledocs):
+        d = datetime.date(int(googledocs_reporting['date'][i][6:10]),int(googledocs_reporting['date'][i][3:5]),int(googledocs_reporting['date'][i][0:2]))
+        data['reporting'].append({
+            "date": str(d)
+
+        })
+    
+    
+    
 
 
-    print(data)
+    print(data['reporting'])
