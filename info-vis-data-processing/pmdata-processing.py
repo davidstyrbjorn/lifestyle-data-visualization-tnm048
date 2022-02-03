4b7ac39ca74d4f2fff55accf8a7e83424d7e4cb0
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-02-03 20:17:13 +0100

    first draft of json generation done!

--- info-vis-data-processing/pmdata-processing.py
+++ info-vis-data-processing/pmdata-processing.py
@@ -1,58 +1,68 @@
 import pandas
-import datetime
+import json
+import numpy as np
 
 N = 16
 
 for i in range(1,N+1):
-
     # Get the correct person id
     person_id = "p"
     if(i <= 9): person_id += ('0' + str(i))
     else: person_id += (str(i))
 
     # Open pmsys data (wellness)
-    pmsys_wellness = pandas.read_csv('pmdata/' + person_id + "/pmsys/wellness.csv")
-    googledocs_reporting = pandas.read_csv('pmdata/' + person_id + "/googledocs/reporting.csv")
+    try:
+        pmsys_wellness = pandas.read_csv('pmdata/' + person_id + "/pmsys/wellness.csv")
+        googledocs_reporting = pandas.read_csv('pmdata/' + person_id + "/googledocs/reporting.csv")
+    except FileNotFoundError:
+        print("Not enough data for: " + person_id)
+        continue
 
     # Data structure for each person    
     data = {
-        "wellness": [
-            
-        ],
-        "reporting": [
-            
+        "life-style": [
+
         ],
         "training_log": [
 
         ]
     }
 
+    # number of entries in each
     measured_days_wellness = len(pmsys_wellness)
     measured_days_googledocs = len(googledocs_reporting)
 
-    # Add measured lifestyle data for each measured day, combine with google docs data. Include dates where both files have data.
+    date_list = googledocs_reporting['date'].tolist();
+    # this will always be something, in the days that bodyweight wasn't logged we will use weight from the last day it was logged
+    current_weight = 0 
+    
+    # Look through each entry here,
     for i in range(measured_days_wellness):
-        data['wellness'].append({
-            "date": pmsys_wellness['effective_time_frame'][i][0:10],
-            "fatigue": pmsys_wellness['fatigue'][i],
-            "mood": pmsys_wellness['mood'][i],
-            "readiness": pmsys_wellness['readiness'][i],
-            "sleep_duration_h": pmsys_wellness['sleep_duration_h'][i],
-            "sleep_quality": pmsys_wellness['sleep_quality'][i],
-            "stress": pmsys_wellness['stress'][i]
-        })
-    
-    # Make separate list for things which are not measured in the same time frame as the wellness data
-    # Check both dates in each row
-    for i in range(measured_days_googledocs):
-        d = datetime.date(int(googledocs_reporting['date'][i][6:10]),int(googledocs_reporting['date'][i][3:5]),int(googledocs_reporting['date'][i][0:2]))
-        data['reporting'].append({
-            "date": str(d)
+        # log if the date exists also in the wellness set
+        for j, google_date in enumerate(date_list):
+            d = google_date[6:10] + '-' + google_date[3:5] + '-' + google_date[0:2] # Convert google date to format YY-MM-DD
+            if str(pmsys_wellness['effective_time_frame'][i][0:10]) == d:
+                # check if bodyweight exists for this entry
+                if not np.isnan(googledocs_reporting['weight'][j]):
+                    current_weight = googledocs_reporting['weight'][j]
 
-        })
-    
-    
-    
+                # we have an entry where date exists in both lists
+                # append to our life-style log json entry
+                data['life-style'].append({
+                    "date": str(d), # the date
+                    "fatigue": int(pmsys_wellness['fatigue'][i]),
+                    "mood": int(pmsys_wellness['mood'][i]),
+                    "readiness": int(pmsys_wellness['readiness'][i]),
+                    "sleep_duration_h": int(pmsys_wellness['sleep_duration_h'][i]),
+                    "sleep_quality": int(pmsys_wellness['sleep_quality'][i]),
+                    "stress": int(pmsys_wellness['stress'][i]),
+                    "glasses_of_fluid": int(googledocs_reporting['glasses_of_fluid'][j]),
+                    "bodyweight": float(current_weight)
+                })
+                break
+        
+    # create output file
+    output = open(person_id + '.json', 'w');
 
-
-    print(data['reporting'])
+    # dump all our json into this file
+    json.dump(data, output)
