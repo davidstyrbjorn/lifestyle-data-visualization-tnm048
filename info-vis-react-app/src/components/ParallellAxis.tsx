760415d88d39f276f3bd0dbdae089e9c32ef4382
Ruben Bromée <tharubb@gmail.com>
2022-02-11 10:27:54 +0100

    Component creation

---
+++ info-vis-react-app/src/components/ParallellAxis.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as d3 from "d3";
+
+
+const ParallellAxisPlot: React.FC<{}> = () => {
+    return (
+        <div id = "plot">
+        </div>
+        
+    );
+} 
