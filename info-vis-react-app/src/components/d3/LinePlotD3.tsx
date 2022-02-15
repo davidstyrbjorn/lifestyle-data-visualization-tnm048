9493a53003a2398e04df86e77637df45b12e713c
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-02-15 22:59:18 +0100

    added dots to line-plot

--- info-vis-react-app/src/components/d3/LinePlotD3.tsx
+++ info-vis-react-app/src/components/d3/LinePlotD3.tsx
@@ -13,23 +13,25 @@ const LinePlotD3: React.FC<{}> = () => {
         if(personData.length === 0) return;
 
         // set the dimensions and margins of the graph
-        var margin = {top: 10, right: 100, bottom: 30, left: 30},
-            width = 700 - margin.left - margin.right,
+        var margin = {top: 100, right: 100, bottom: 30, left: 100},
+            width = 900 - margin.left - margin.right,
             height = 600 - margin.top - margin.bottom;
 
-        let svg: any = document.getElementsByClassName('line-plot-svg');
-        console.log(svg);
-        if(svg.length === 0){
-            svg = d3.select('#my_dataviz')
+        // If we haven't added the svg before
+        let previous_svg: any = document.getElementsByClassName('line-plot-svg');
+        if(previous_svg.length === 0){
+            d3.select('#my_dataviz')
                 .append('svg')
                     .attr('width', width + margin.left + margin.right)
                     .attr('height', height + margin.top + margin.bottom)
                     .attr('class', 'line-plot-svg')
                 .append('g')
                     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-        }else{
-            svg = d3.select('.line-plot.svg');
         }
+
+        // Get the svg
+        let svg = d3.select('.line-plot-svg');
+        svg.selectAll("*").remove();
         
         // 2019-11-17
         let timeParser = d3.timeParse("%Y-%m-%d");
@@ -40,21 +42,27 @@ const LinePlotD3: React.FC<{}> = () => {
             dates.push(timeParser(entry.date)!)
         })
 
+        // Create the x-scale
         let x = d3.scaleTime()
             // @ts-ignore
             .domain(d3.extent(dates))
-            .range([0, width]); 
+            .range([0, width]);
+        // Add the x-scale to DOM
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x));
 
-        let maxY = d3.max(data, function(d) {return d.mood});
+        // We extract maxY for code-reading reasons!
+        let maxY = d3.max(data, function(d) {return d.sleep_duration_h});
+        // Create the y-scale
         let y = d3.scaleLinear()
             .domain([0, maxY!])
             .range([height, 0])
+        // Add the y-scale to the DOM
         svg.append("g")
             .call(d3.axisLeft(y));
 
+        // Now add all our lines from the data
         svg.append('path')
             .datum(data)
             .attr('fill', 'none')
@@ -62,8 +70,19 @@ const LinePlotD3: React.FC<{}> = () => {
             .attr('stroke-width', 1.5)
             .attr('d', d3.line<lifestyle>()
                 .x(function(d) {return (x(timeParser(d.date)!))})
-                .y(function(d) {return y(d.mood)})
-            )
+                .y(function(d) {return y(d.sleep_duration_h)})
+            );
+
+        // Now add all our dots
+        svg.selectAll('myCircles')
+            .data(data)
+            .enter()
+            .append('circle')
+                .attr('fill', 'red')
+                .attr('stroke', 'none')
+                .attr('cx', (d) => x(timeParser(d.date)!))
+                .attr('cy', (d) => y(d.sleep_duration_h))
+                .attr('r', 2)
 
     }, [personData]);
 
