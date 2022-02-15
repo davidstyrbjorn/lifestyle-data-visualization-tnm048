05d85f9d4c3b672fe2783cfb68a05ff2051f307b
Ruben Bromée <tharubb@gmail.com>
2022-02-15 09:56:19 +0100

    Reading data from personstate

--- info-vis-react-app/src/components/d3/ParallellAxis.tsx
+++ info-vis-react-app/src/components/d3/ParallellAxis.tsx
@@ -1,53 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as d3 from "d3"
 import useD3  from "../../hooks/useD3";
 import { setOriginalNode } from "typescript";
+import availablePerson, {filteredPersonData} from "../../states/person-state";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 const ParallellAxisPlot: React.FC<{}> = () => {
+
+    const data = useRecoilValue(filteredPersonData);
+
+    // Update axis depending on data, useD3 handles like useEffect
     const ref = useD3((svg: any) => {
+        // Draw the canvas
         const margin = {top: 50, right: 50, bottom: 50, left: 50}
         const width = 600 - margin.left - margin.right;
         const height =  600 - margin.top - margin.bottom;
         d3.select("plot")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
-        .attr("height", width + margin.top + margin.bottom)
+        .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        const data = require("../../p01.json");
-        console.log(data["life-style"]);
-        const dimensions = Object.keys(data["life-style"][0]["date"]);
-        console.log(dimensions);
-        const sortedData = data["life-style"].sort((a: any, b: any) => a.fatigue - b.fatigue);
-        console.log(sortedData);
-        const min = data["life-style"].reduce(function (prev: any, curr: any) {
-            return prev.fatigue < curr.fatigue ? prev : curr;
-        });
-        console.log(min.fatigue);
-        const y = {}
-
-        // https://www.d3-graph-gallery.com/graph/parallel_basic.html
-        for (const i in dimensions) {
-            const name = dimensions[i];
-
-
-            // y[name] = d3.scaleLinear()
-            // .domain(d3.extent)
+        if (data.length !== 0) {
+            console.log("D3", data);
+            const attributes = Object.keys(data[0].lifestyle[0]);
+            const y:any = {}
+            // https://www.d3-graph-gallery.com/graph/parallel_basic.html
+            for (var i = 1; i < attributes.length; i++) {
+                const name = attributes[i]; // Getting names of attributes
+                const min:any = data[0].lifestyle.reduce(function (prev: any, curr: any) {
+                    return prev[name] < curr[name] ? prev : curr;
+                });
+                const max:any = data[0].lifestyle.reduce(function (prev: any, curr: any) {
+                    return prev[name] > curr[name] ? prev : curr;
+                });
+                y[name] = d3.scaleLinear()
+                .domain([min, max])
+                .range([height, 0])
+            }
+            console.log(y)
         }
-    }, [] )
+    }, [data] )
+
 
 
     return (
-    
+        <div>
+
+        </div>
+        /*
         <svg ref = {ref}>
             <div id = "plot">
                 Test
             </div>
-        </svg>
+        </svg>*/
     );
-
-    
 } 
 
 export default ParallellAxisPlot;
